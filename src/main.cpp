25aba886bfe557fb19c7d0692e90deb9e5acd29c
Dominic Cupo <Dominic.cupo@gmail.com>
2017-11-22 22:15:12 -0500

    Added the main file

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by djcupo on 11/22/17.
+//
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+int main(int argc, char* argv[]){
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
