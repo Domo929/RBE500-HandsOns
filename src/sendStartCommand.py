63113c40eedf9bdc528ab769fb9e776b5fa96175
Dominic Cupo <Dominic.cupo@gmail.com>
2017-11-24 01:05:09 -0500

    Added a python script to send the launch command to the Face Detection node

---
+++ src/sendStartCommand.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import Bool
+
+def talker():
+    pub = rospy.Publisher('startFaceDetection', Bool, queue_size=10)
+    rospy.init_node('ManualStart', anonymous=True)
+    while not rospy.is_shutdown():
+        command = raw_input("Press Enter to send start command, or q to exit\n")
+        if command is 'q':
+            break
+        else:
+          pub.publish(True)
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
