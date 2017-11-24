233796e331c898ae78f26854a263e6370adabe5a
Dominic Cupo <Dominic.cupo@gmail.com>
2017-11-24 01:07:36 -0500

    Created another script to send unlock command

---
+++ src/sendUnlockCommand.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Bool
+
+def talker():
+    pub = rospy.Publisher('unlock', Bool, queue_size=10)
+    rospy.init_node('ManualStart', anonymous=True)
+    while not rospy.is_shutdown():
+        command = raw_input("Press Enter to send unlock command, or q to exit\n")
+        if command is 'q':
+            break
+        else:
+          pub.publish(True)
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
